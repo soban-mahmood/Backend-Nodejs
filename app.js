bde18b2a3ada519265f33862633b0816356ed471
soban-mahmood <sobanmehmood43@gmail.com>
2024-12-25 11:35:50 +0500

    backend

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const PORT = 9000
+
+const server = http.createServer((req,res) => {
+    
+  if(req.url === "/"){
+    res.end("i am home page")
+  }
+  if(req.url === "/about"){
+    res.end("i am about page hello world")
+  }
+}
+)
+
+server.listen(PORT)

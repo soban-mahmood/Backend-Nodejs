8310c446d56bd4a1f2ac1ed1ec06b15b8b6c0c49
soban-mahmood <sobanmehmood4343@gmail.com>
2024-12-26 12:08:21 +0500

    express routing / middlewere and Views engine

--- express.js
+++ express.js
@@ -1,8 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.set("view engine", "ejs");
 
-app.listen(3000)
+app.use((req, res, next) => {
+  console.log("i am middlewere");
+  return next();
+});
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+app.get("/about", function (req, res) {
+  res.render("index");
+});
+app.get("/contact", function (req, res) {
+  res.render("index");
+});
+app.post("/post", (req, res) => {
+  res.send("POST request to the homepage");
+});
+app.listen(port, () => {
+  console.log(`server is runing on port ${port}`);
+});
